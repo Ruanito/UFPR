1a0392236a5f0b8e5a8255c0bd257ae03bd39244
Ruanito Diego <ruanitodiego@gmail.com>
2015-06-24 19:41:39 -0300

    Envia mensagem para o arduino

--- CI306-Android_II/CI306-Final/app/src/main/java/br/ufpr/inf/rds11/ci306_final/Main.java
+++ CI306-Android_II/CI306-Final/app/src/main/java/br/ufpr/inf/rds11/ci306_final/Main.java
@@ -2,14 +2,19 @@ package br.ufpr.inf.rds11.ci306_final;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,18 +27,29 @@ public class Main extends ActionBarActivity {
     private static final String TAG = "Jon";
     private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     private BluetoothSocket btSocket = null;
+    private BluetoothServerSocket bluetoothServerSocket;
     private OutputStream outStream = null;
     private InputStream inStream = null;
+    private Server server = null;
+    private String dataToSend;
 
     private static final int REQUEST_ENABLE = 0x1;
     private static final int REQUEST_DISCOVERABLE = 0x2;
     private static String address = "20:13:01:24:00:76";
     private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
+    Handler handler = new Handler();
+    byte delimiter = 10;
+    boolean stopWorker = false;
+    int readBufferPosition = 0;
+    byte[] readBuffer = new byte[1024];
+    TextView Result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Result = (TextView) findViewById(R.id.result);
     }
 
     @Override
@@ -84,5 +100,100 @@ public class Main extends ActionBarActivity {
             }
             Log.d(TAG, "Socket creating failed");
         }
+    }
+
+    public void onSendDataButtonClicked(View view) {
+        dataToSend = "5";
+        writeData(dataToSend);
+        beginListenForData();
+    }
+
+
+    private void writeData(String data) {
+        try {
+            outStream = btSocket.getOutputStream();
+        } catch (IOException e) {
+            Log.d(TAG, "Bug BEFORE Sending stuff", e);
+        }
+
+        String message = data;
+        byte[] msgBuffer = message.getBytes();
+
+        try {
+            outStream.write(msgBuffer);
+        } catch (IOException e) {
+            Log.d(TAG, "Bug while sending stuff", e);
+        }
+    }
+
+    public void beginListenForData()   {
+        try {
+            inStream = btSocket.getInputStream();
+        } catch (IOException e) {
+            Log.d(TAG, "Ruanito", e);
+        }
+
+        Thread workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        int bytesAvailable = inStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            inStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "UTF-8");
+                                    readBufferPosition = 0;
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+
+                                            if(Result.getText().toString().equals("..")) {
+                                                Result.setText(data);
+                                            } else {
+                                                Result.append("\n"+data);
+                                            }
+
+                                                        /* You also can use Result.setText(data); it won't display multilines
+                                                        */
+                                            Result.setText(data);
+                                            Context context = getApplicationContext();
+                                            CharSequence text = "Hello toast!";
+                                            int duration = Toast.LENGTH_SHORT;
+
+                                            Toast toast = Toast.makeText(context, data, duration);
+                                            toast.show();
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        Log.d(TAG, "Diego", ex);
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
     }
 }
---
+++ CI306-Android_II/CI306-Final/app/src/main/java/br/ufpr/inf/rds11/ci306_final/Server.java
@@ -0,0 +1,59 @@
+package br.ufpr.inf.rds11.ci306_final;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * Created by ruanito on 24/06/15.
+ */
+public class Server extends Thread {
+    private final BluetoothServerSocket mmServerSocket;
+    private BluetoothAdapter mBluetoothAdapter;
+    private static String address = "20:13:01:24:00:76";
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    public Server() {
+        // Use a temporary object that is later assigned to mmServerSocket,
+        // because mmServerSocket is final
+        BluetoothServerSocket tmp = null;
+        try {
+            // MY_UUID is the app's UUID string, also used by the client code
+            tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord(address, MY_UUID);
+        } catch (IOException e) { }
+        mmServerSocket = tmp;
+    }
+
+    public void run() {
+        BluetoothSocket socket = null;
+        // Keep listening until exception occurs or a socket is returned
+        while (true) {
+            try {
+                socket = mmServerSocket.accept();
+            } catch (IOException e) {
+                break;
+            }
+            // If a connection was accepted
+            if (socket != null) {
+                // Do work to manage the connection (in a separate thread)
+                //manageConnectedSocket(socket);
+                try {
+                    mmServerSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+    }
+
+    /** Will cancel the listening socket, and cause the thread to finish */
+    public void cancel() {
+        try {
+            mmServerSocket.close();
+        } catch (IOException e) { }
+    }
+}
