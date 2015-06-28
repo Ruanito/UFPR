58637d32ce2c3d07d4f8a3cd9f8d8b5f5cb50881
rogeriosm91 <rogeriosm91@gmail.com>
2015-06-27 21:30:31 -0300

    fiz magica

---
+++ CI306-Android_II/CI306-Final/app/src/main/java/br/ufpr/inf/rds11/ci306_final/ManualControl.java
@@ -0,0 +1,14 @@
+package br.ufpr.inf.rds11.ci306_final;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Rogerio on 26/06/2015.
+ */
+public class ManualControl extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
