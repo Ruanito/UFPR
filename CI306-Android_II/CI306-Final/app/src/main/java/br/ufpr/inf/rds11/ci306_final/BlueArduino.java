71102be3308f95d5b80b60b057306a509e54dd5f
Ruanito Diego <ruanitodiego@gmail.com>
2015-06-28 19:42:28 -0300

    Adicionado funcionalidades do bottão

--- CI306-Android_II/CI306-Final/app/src/main/java/br/ufpr/inf/rds11/ci306_final/BlueArduino.java
+++ CI306-Android_II/CI306-Final/app/src/main/java/br/ufpr/inf/rds11/ci306_final/BlueArduino.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 
@@ -48,7 +49,7 @@ public class BlueArduino extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onSendMessageButtonClicked(View view) {
+    public void onSendMessageButtonClicked(View view, MotionEvent event) {
         String message = newMessage.getText().toString();
         mBlue.SendMessage(message);
         mBlue.getLastMessage();
--- CI306-Android_II/CI306-Final/app/src/main/java/br/ufpr/inf/rds11/ci306_final/ManualControl.java
+++ CI306-Android_II/CI306-Final/app/src/main/java/br/ufpr/inf/rds11/ci306_final/ManualControl.java
@@ -2,14 +2,60 @@ package br.ufpr.inf.rds11.ci306_final;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by Rogerio on 26/06/2015.
  */
 public class ManualControl extends ActionBarActivity {
+
+    private BluetoothArduino mBlue = null;
+    private ImageButton buttonUp = null;
+    private ImageButton buttonRotLeft = null;
+    private ImageButton buttonRotRight = null;
+    private ImageButton buttonDown = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manual_control);
+
+        mBlue = BluetoothArduino.getInstance("linvor");
+
+        mBlue.Connect();
+
+        buttonUp = (ImageButton) findViewById(R.id.buttonUp);
+        buttonRotLeft = (ImageButton) findViewById(R.id.buttonRotLeft);
+        buttonRotRight = (ImageButton) findViewById(R.id.buttonRotRight);
+        buttonDown = (ImageButton) findViewById(R.id.buttonDown);
+
+        buttonUp.setOnLongClickListener(onLongClickListener);
+        buttonRotLeft.setOnLongClickListener(onLongClickListener);
+        buttonRotRight.setOnLongClickListener(onLongClickListener);
+        buttonDown.setOnLongClickListener(onLongClickListener);
+
     }
+
+    private View.OnLongClickListener onLongClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            switch (v.getId()){
+                case R.id.buttonUp:
+                    mBlue.SendMessage("5");
+                    break;
+                case R.id.buttonRotLeft:
+                    mBlue.SendMessage("E");
+                    break;
+                case R.id.buttonRotRight:
+                    mBlue.SendMessage("F");
+                    break;
+                case R.id.buttonDown:
+                    mBlue.SendMessage("2");
+                    break;
+            }
+            return false;
+        }
+    };
 }
