83ec5fef7eb176b2570e4f08484d04cd93ed4b88
rogeriosm91 <rogeriosm91@gmail.com>
2015-06-28 21:16:03 -0300

    button

--- CI306-Android_II/CI306-Final/app/src/main/java/br/ufpr/inf/rds11/ci306_final/ManualControl.java
+++ CI306-Android_II/CI306-Final/app/src/main/java/br/ufpr/inf/rds11/ci306_final/ManualControl.java
@@ -2,7 +2,10 @@ package br.ufpr.inf.rds11.ci306_final;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.MotionEvent;
+
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -19,8 +22,28 @@ public class ManualControl extends ActionBarActivity {
         setContentView(R.layout.manual_control);
 
         mBlue = BluetoothArduino.getInstance("linvor");
-
         mBlue.Connect();
+
+        Button buttonUp = (Button) findViewById(R.id.buttonUp);
+
+        buttonUp.setOnTouchListener(new OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    String message = "8";
+                    mBlue.SendMessage(message);
+
+                }
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    String message = "0";
+                    mBlue.SendMessage(message);
+                }
+                return true;
+            }
+
+        });
     }
 
     public void onDownButtonClicked(View view) {
